3df183e41f9888f5949465e33b707ca1172123e9
VictorCortez358 <00022120@uca.edu.sv>
2021-03-12 19:11:06 -0600

    Proof

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CountersandAccumulators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.Write("Insert n:");
+           int n= Int32.Parse(Console.ReadLine());
+           
+
+           for(int altura= 1; altura<=n;altura++)
+           {
+               for(int espacio=n; espacio > altura;espacio--)
+               {
+                   Console.Write(espacio);
+                   
+               }
+               for(int i=1; i<=(altura*2);i++)
+               {
+                   Console.Write(" ");
+               }
+               Console.WriteLine(" ");
+
+           }  
+        }     
+   }
+}
+
